045e909b0c0ee32dc3d3fb2272a9fd58edff7c55
iflasch <ido@uopday.com>
2021-05-30 10:05:35 +0300

    first commit

---
+++ src/test/kotlin/com/aikit/routing/RoutingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aikit.routing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RoutingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
